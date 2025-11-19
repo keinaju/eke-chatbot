4bb6256dc0e0676f902e77a58cde53b722f928bc
Jukka Keinanen <jukka.keinane@gmail.com>
2025-11-19 09:31:27 +0200

    Connect frontend to backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,18 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (request: Request, response: Response) => {
-  response.send("Hello X!");
+app.get('/', (request: Request, response: Response) => {
+  response.send('Hello world!');
+});
+
+app.get('/api/hello', (request: Request, response: Response) => {
+  response.json({ message: 'Hello API!' });
 });
 
 app.listen(port, () => {
